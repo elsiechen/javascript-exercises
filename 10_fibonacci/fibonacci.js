e72cb55c5f191c6cdc89190e427d431e7ce4506a
elsie <elsiechen915@gmail.com>
2022-08-24 23:44:39 -0400

    Exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,12 @@
-const fibonacci = function() {
+const fibonacci = function(index) {
+    if(index < 0) return 'OOPS';
 
+    const array = [];
+    array[0] = array[1] = 1;
+    for(let i = 2; i < 100; i++){
+        array[i] = array[i - 1] + array[i - 2];
+    }
+    return array[Number(index - 1)];
 };
 
 // Do not edit below this line
