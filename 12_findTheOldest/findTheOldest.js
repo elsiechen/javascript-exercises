42d8f7d6ac378848a38077e02e6e49032f4505e2
elsie <elsiechen915@gmail.com>
2022-08-25 22:18:25 -0400

    Exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,13 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(array) {
+    
+    let sorted = array.sort((a,b) => {
+        if(!a['yearOfDeath'] || !b['yearOfDeath']){
+            a['yearOfDeath'] = b['yearOfDeath'] = new Date().getFullYear();
+        }
+        return (a['yearOfDeath'] - a['yearOfBirth']) > (b['yearOfDeath'] - b['yearOfBirth']) ? -1: 1;
+    });
+    return sorted[0];
+    
 };
 
 // Do not edit below this line
